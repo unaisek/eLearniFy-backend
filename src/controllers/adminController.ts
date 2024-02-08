7fa68df8e8622ab6aa8a9a7631bb94d45d557568
unaisek <mohammedunaiseknkt@gmail.com>
2024-02-08 14:58:10 +0530

    admin side enrolled course list added

--- src/controllers/adminController.ts
+++ src/controllers/adminController.ts
@@ -1,12 +1,15 @@
 import { NextFunction, Request, Response } from "express";
 import AdminService from "../services/adminService";
+import CourseService from "../services/CourseService";
 
 
 export default class AdminController {
 
     private _adminService:AdminService
+    private _courseService:CourseService
     constructor() {
         this._adminService = new AdminService()
+        this._courseService = new CourseService()
     }
 
     
@@ -20,6 +23,20 @@ export default class AdminController {
 
         } catch (error) {
         next(error);
+        }
+    }
+
+    async getAllEnrolledCourses(req:Request, res:Response, next:NextFunction){
+        try {
+            const year = parseInt(req.params.year)
+            
+            const enrolledCourses = await this._courseService.getAllEnrolledCourse(year);
+            if(enrolledCourses){
+                res.status(200).json(enrolledCourses)
+            }
+            
+        } catch (error) {
+            next(error)
         }
     }
 }
